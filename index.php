021ffee3c8d73dc5c0b76c70d943dc606274bcd7
gllamoja <gllamoja@gmail.com>
2014-08-16 12:35:05 -0500

    figure

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
     <title>Alerta pc </title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/main.css" rel="styleshet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -28,6 +29,9 @@
     <div class="navbar navbar-inverse navbar-static-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
+          <figure id="logo">
+      <img src="logo.png" />
+    </figure>
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>

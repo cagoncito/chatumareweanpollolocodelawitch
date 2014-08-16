70aa9ab065e0aac261acd35a92da93d2a5937441
gllamoja <gllamoja@gmail.com>
2014-08-16 13:12:03 -0500

    asas

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="/css/main.css" rel="styleshet">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+ <link href="/css/bootstrap.min.css" rel="stylesheet">
     
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
